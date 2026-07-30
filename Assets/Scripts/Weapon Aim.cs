48814c57feceef8c467968ea44850dd2289597f3
W41D0 <walidogamer48@gmail.com>
2026-07-31 02:11:56 +0300

    floaty rotation

--- Assets/Scripts/Weapon Aim.cs
+++ Assets/Scripts/Weapon Aim.cs
@@ -4,9 +4,20 @@ using UnityEngine.InputSystem;
 
 public class WeaponAim : MonoBehaviour
 {
+    [Header("Floaty Settings")]
+    [SerializeField] Transform playerTransform;
+    [SerializeField] float radius = 0.25f;
+    [SerializeField] float gunFollowSpeed = 15f;
+    [SerializeField] float gunRotateSpeed = 12f;
+
+
+
     Vector2 mouseScreenPosition;
     Vector3 mouseWorldPosition;
+    Vector3 directionFromPlayer;
+    Vector3 targetPosition;
     Vector3 aimDirection;
+    Quaternion targetRotation;
     float angle;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -21,11 +32,16 @@ public class WeaponAim : MonoBehaviour
         mouseWorldPosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
         mouseWorldPosition.z = 0;
 
-        aimDirection = mouseWorldPosition - transform.position;
+        directionFromPlayer = mouseWorldPosition - playerTransform.position;
+        targetPosition = Vector3.ClampMagnitude(directionFromPlayer, radius);
+        transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, gunFollowSpeed * Time.deltaTime);
 
+        aimDirection = mouseWorldPosition - transform.position;
         angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
 
-        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        
+        targetRotation = Quaternion.Euler(0f, 0f, angle);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, gunRotateSpeed * Time.deltaTime);
     }
 
     void OnAim(InputValue value)
