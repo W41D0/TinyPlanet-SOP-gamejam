17a952e1a0cbcb65a1942b62f6e58c2c8a013b4b
W41D0 <walidogamer48@gmail.com>
2026-07-31 16:41:09 +0300

    bullets spawn correctly and move through the air

--- Assets/Scripts/Gun Script.cs
+++ Assets/Scripts/Gun Script.cs
@@ -1,11 +1,17 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class GunScript : MonoBehaviour
 {
+    [Header("Debug")]
+    [SerializeField] Transform bulletSpawnObject;
+    
+    Vector2 bulletSpawnPosition;
+    Quaternion bulletSpawnRotation;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        bulletSpawnObject = transform.Find("Bullet Spawn").GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -16,6 +22,10 @@ public class GunScript : MonoBehaviour
 
     public void Shoot(GameObject Bullet, float speed)
     {
-
+        bulletSpawnPosition = bulletSpawnObject.position;
+        bulletSpawnRotation = bulletSpawnObject.rotation;
+        GameObject spawnedBullet = Instantiate(Bullet, bulletSpawnPosition, bulletSpawnRotation);
+        Rigidbody2D bulletRB = spawnedBullet.GetComponent<Rigidbody2D>();
+        bulletRB.linearVelocity = spawnedBullet.transform.up * speed;
     }
 }
--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -1,20 +1,32 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    GunScript gun;
+
+    [Header("Bullets")]
+    [SerializeField] GameObject solidBullet;
+    [SerializeField] float solidBulletSpeed = 10f;
+    [SerializeField] float timeBetweenSolidBullets = 0.2f;
+
     Rigidbody2D rb;
     Vector2 moveDirection;
     [SerializeField] float baseMoveSpeed = 5f;
     [SerializeField] float SprintMultiplier = 1.5f;
     float moveSpeed;
     Boolean isSprinting;
+    Boolean isShooting;
+    Boolean isOnShootCooldown;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Transform gunTransform = transform.Find("Weapon Pivot/Gun");
+        gun = gunTransform.GetComponent<GunScript>();
         rb = gameObject.GetComponent<Rigidbody2D>();
         moveSpeed = baseMoveSpeed;
     }
@@ -23,6 +35,11 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         rb.linearVelocity = moveDirection * moveSpeed;
+        if(isShooting && !isOnShootCooldown)
+        {
+            StartCoroutine(shootMethod(timeBetweenSolidBullets));
+        }
+
     }
 
     void OnMove(InputValue value)
@@ -42,5 +59,25 @@ public class PlayerController : MonoBehaviour
             moveSpeed = baseMoveSpeed;
             isSprinting = false;
         }
+    }
+
+    void OnFire(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            isShooting = true;
+        }
+        else
+        {
+            isShooting = false;
+        }
+    }
+
+    IEnumerator shootMethod(float shootCooldown)
+    {
+        gun.Shoot(solidBullet, solidBulletSpeed);
+        isOnShootCooldown = true;
+        yield return new WaitForSeconds(shootCooldown);
+        isOnShootCooldown = false;
     }
 }
