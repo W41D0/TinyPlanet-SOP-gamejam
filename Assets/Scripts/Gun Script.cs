04101eae441bca2f66021a4f85c9dae6e1728e41
Amin1KM <aminna131@gmail.com>
2026-08-04 13:54:31 +0300

    sss

--- Assets/Scripts/Gun Script.cs
+++ Assets/Scripts/Gun Script.cs
@@ -1,4 +1,3 @@
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using System.Collections;
 using UnityEngine.Assemblies;
@@ -116,7 +115,7 @@ public class GunScript : MonoBehaviour
     Transform bulletSpawnObject;
 
     string currentState = "s";
-    bool inPlasmaMode = false;
+    //bool inPlasmaMode = false;
     private PlayerStats stats;
 
     PlayerController player;
