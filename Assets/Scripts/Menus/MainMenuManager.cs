92f5449643acbf02021eb987ba6cc32bb625541d
Amin1KM <aminna131@gmail.com>
2026-08-04 11:49:10 +0300

    mooooore stuff panel and work on waldo test scene

--- Assets/Scripts/Menus/MainMenuManager.cs
+++ Assets/Scripts/Menus/MainMenuManager.cs
@@ -3,14 +3,52 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [Header("Main Menus")]
     public GameObject mainMenuPanel; 
     public GameObject controlsPanel; 
+    
+    [Header("Story/Info Panels Sequence")]
+    public GameObject panel1;
+    public GameObject panel2;
+    public GameObject panel3;
+    public GameObject panel4;
     public GameObject stuffpanel;
+    
+    [Header("Settings")]
     public string gameSceneName = "Level1"; 
 
     public void PlayGame()
     {   
+        if (panel1 != null) panel1.SetActive(true);
+    }
+
+    public void GoToPanel2()
+    {
+        if (panel1 != null) panel1.SetActive(false);
+        if (panel2 != null) panel2.SetActive(true);
+    }
+
+    public void GoToPanel3()
+    {
+        if (panel2 != null) panel2.SetActive(false);
+        if (panel3 != null) panel3.SetActive(true);
+    }
+
+    public void GoToPanel4()
+    {
+        if (panel3 != null) panel3.SetActive(false);
+        if (panel4 != null) panel4.SetActive(true);
+    }
+
+    public void GoToStuffPanel()
+    {
+        if (panel4 != null) panel4.SetActive(false);
         if (stuffpanel != null) stuffpanel.SetActive(true);
+    }
+
+    public void okiee()
+    {
+        SceneManager.LoadScene(gameSceneName);
     }
 
     public void ShowControlsPanel()
@@ -23,11 +61,14 @@ public class MainMenuManager : MonoBehaviour
     {
         if (mainMenuPanel != null) mainMenuPanel.SetActive(true);
         if (controlsPanel != null) controlsPanel.SetActive(false);
+        
+        if (panel1 != null) panel1.SetActive(false);
+        if (panel2 != null) panel2.SetActive(false);
+        if (panel3 != null) panel3.SetActive(false);
+        if (panel4 != null) panel4.SetActive(false);
+        if (stuffpanel != null) stuffpanel.SetActive(false);
     }
-    public void okiee()
-    {
-        SceneManager.LoadScene(gameSceneName);
-    }
+
     public void QuitGame()
     {
         Debug.Log("Game is exiting...");
